ea4bcbb5b884477b6dfd41804c02493b677104fc
fatimah-learn <68842990+fatimah-learn@users.noreply.github.com>
2020-08-11 17:03:23 +0300

    Update: add example in array-map-function.js

--- week04/day03/array-map-function.js
+++ week04/day03/array-map-function.js
@@ -7,3 +7,16 @@ let result = numbers.map(function(value){
 let result2 = numbers.map(value => value*2); // for one line implementation
 console.log(result);
 console.log(result2);
+
+
+
+// Another example about MAP in Arrays
+
+
+let prices = [10 , 20 , 30 , 45 , 57];
+let pricesAfterTax = prices.map(function(value){
+
+    return value * 0.15 + value ;
+})
+
+console.log(pricesAfterTax);
