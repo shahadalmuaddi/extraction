e810379c64e2038463b0d4aaf869279f0371e4f4
Reem <68843028+reemlearn@users.noreply.github.com>
2020-08-11 17:18:15 +0300

    Added another array filter example

--- week04/day03/array-filter.js
+++ week04/day03/array-filter.js
@@ -2,5 +2,9 @@ let numbers = [1 , 3 ,4 ,5 ];
 let resault = numbers.filter(function(value){
 return value >= 3;
 });
-
-consol.log(resault);
+let mixedArray = [1,"Welcome",2,3,"To",4,5,"JavaScript Course"];
+let result2 = mixedArray.filter(function(value){
+    return (typeof value)=='string';
+});
+console.log(resault);
+console.log(result2);
